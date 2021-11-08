afd4d0a2d2beb741a3058d84713cbda02a71e51f
Manitotre <parismilonas97@gmail.com>
2021-11-08 15:13:20 +0100

    Removed unhelpful lines from the script.

--- wallet.py
+++ wallet.py
@@ -1,7 +1,6 @@
 # wallet.py
 
 class InsufficientAmount(Exception):
-    print('Insufficient Amount')
     pass
 
 
@@ -9,17 +8,12 @@ class Wallet(object):
     def __init__(self, initial_amount=0, balance=0):
         self.initial_amount=initial_amount
         self.balance=initial_amount
-        #raise NotImplementedError
 
     def spend_cash(self, amount):
         if self.balance<amount: 
             raise(InsufficientAmount)
         else:
             self.balance -= amount
-            return self.balance
-       # raise NotImplementedError
 
     def add_cash(self, amount):
         self.balance += amount
-        return self.balance
-        #raise NotImplementedError
